26d95d3b005e4780bd95d9ffffde69cfd004e6a1
Alex Arica <alex.arica@reactive-tech.io>
2021-06-17 12:04:09 +0100

    The deployment script extends the base deployment script making it smaller, without duplications and easier to maintain. Moved the websites related files in the new folder 'website'.

--- deploy.sh
+++ deploy.sh
@@ -1,43 +1,15 @@
 #!/bin/bash
 
-source ~/source/kubernetes-cluster/local-cluster/util/deploymentUtilities.sh
-
-exitIfEnvArgIsMissing $1
-exitIfVersionArgIsMissing $2
-
-BUILD_ENV=$1
-BUILD_VERSION=$2
-REBUILD_DEV_IMAGE=$3
-
 BUILD_IMG_DEPLOYMENT_NAME="kubegres-website"
 BUILD_IMG_NAMESPACE="static-websites"
-BUILD_IMG="reactivetechio/$BUILD_IMG_DEPLOYMENT_NAME:$BUILD_VERSION"
 
-if [ "$BUILD_ENV" == "dev" ]; then
-  BUILD_IMG="localhost:5000/$BUILD_IMG"
-fi
+source ~/source/kubernetes-cluster/deployment/baseDeploy.sh
 
-echo "Deploying $BUILD_IMG in ${BUILD_ENV} environment"
+function copyDevOutput() {
+  echo "Copying dev output to $BUILD_TARGET_PATH"
+  gigo deployment/Gigo.yaml
+  cp deployment/Dockerfile $BUILD_TARGET_PATH
+  cp -rp deployment/nginx $BUILD_TARGET_PATH
+}
 
-gigo deployment/Gigo.yaml
-cp deployment/Dockerfile /usr/share/dev-output/$BUILD_IMG_NAMESPACE/$BUILD_IMG_DEPLOYMENT_NAME
-
-if [ "$BUILD_ENV" == "prod" ]; then
-  buildAndDeployedDockerImg $BUILD_IMG $BUILD_IMG_DEPLOYMENT_NAME $BUILD_IMG_NAMESPACE
-
-elif [ "$BUILD_ENV" == "dev" ]; then
-
-  if ! isDevImageLocallyDeployed $BUILD_IMG $BUILD_IMG_DEPLOYMENT_NAME $BUILD_IMG_NAMESPACE; then
-
-    buildAndDeployedDockerImg $BUILD_IMG $BUILD_IMG_DEPLOYMENT_NAME $BUILD_IMG_NAMESPACE
-    echo "To deploy the DEV image, we are patching the Deployment resource '$BUILD_IMG_DEPLOYMENT_NAME' in the namespace: '$BUILD_IMG_NAMESPACE'"
-    kubectl patch deployment $BUILD_IMG_DEPLOYMENT_NAME -n $BUILD_IMG_NAMESPACE --patch "$(cat deployment/dev/deployment-patch.yaml)"
-
-  elif [ "$REBUILD_DEV_IMAGE" == "r" ]; then
-
-    buildAndDeployedDockerImg $BUILD_IMG $BUILD_IMG_DEPLOYMENT_NAME $BUILD_IMG_NAMESPACE
-    echo "To deploy the latest DEV image, we are restarting the Deployment resource '$BUILD_IMG_DEPLOYMENT_NAME' in the namespace: '$BUILD_IMG_NAMESPACE'"
-    kubectl rollout restart deployment $BUILD_IMG_DEPLOYMENT_NAME -n $BUILD_IMG_NAMESPACE
-  fi
-
-fi
+deploy
