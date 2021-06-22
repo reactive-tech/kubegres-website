f617a0a7e0db24c540dd36f6154a1384e9f81a89
Alex Arica <alex.arica@reactive-tech.io>
2021-06-22 20:15:23 +0100

    Updated deploy.sh to be compatible with the latest baseDeploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
+source ~/source/kubernetes-cluster/deployment/baseDeploy.sh
+
 BUILD_IMG_DEPLOYMENT_NAME="kubegres-website"
 BUILD_IMG_NAMESPACE="static-websites"
 
-source ~/source/kubernetes-cluster/deployment/baseDeploy.sh
-
-function copyDevOutput() {
-  echo "Copying dev output to $BUILD_TARGET_PATH"
+function buildAppAndCopyOutput() {
   gigo deployment/Gigo.yaml
   cp deployment/Dockerfile $BUILD_TARGET_PATH
   cp -rp deployment/nginx $BUILD_TARGET_PATH
