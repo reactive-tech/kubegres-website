cfcead8f01c840eb5340d39fcffa08ce5664b8c2
Alex Arica <alex.arica@reactive-tech.io>
2021-06-24 13:02:06 +0100

    Using the latest baseDeploy.sh and moved the deployment patch to the kubernetes-cluster repo.

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-source ~/source/kubernetes-cluster/deployment/baseDeploy.sh
+source ~/source/kubernetes-cluster/deployment/app/baseDeploy.sh
 
 BUILD_IMG_DEPLOYMENT_NAME="kubegres-website"
 BUILD_IMG_NAMESPACE="static-websites"
